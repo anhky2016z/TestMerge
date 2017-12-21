3a1b5fee975d6cb66041c1c832d1a703032afd93
Anh Kỳ <ky.ngu@gosu.vn>
2017-12-21 16:10:35 +0700

    Test 2

--- TestMerge/Assets/Tesst.cs
+++ TestMerge/Assets/Tesst.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tesst : MonoBehaviour {
+public class Tesst : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void TestFuntion()
+    {
+        int a = 5;
+        int b = 5;
+        int C = 5;
+        int d = 5;
+        int e = 5;
+    }
+
 }
